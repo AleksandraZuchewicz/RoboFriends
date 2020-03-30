40fc0f0d14a461f5f3fd95447731f0d7bd889944
aleksandra.zuchewicz <aleksandra.zuchewicz@gmail.com>
2020-03-30 15:27:09 +0100

    added docs folder to create github page

---
+++ docs/precache-manifest.2f7cb37e3c8411599792311f58b682b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fc0b03d9946fbaad35ae81079e3ada6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dad70c6b4f393277e7d2",
+    "url": "./static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "42f58b9d9e23af4e3cdf",
+    "url": "./static/css/main.d5b0a9a2.chunk.css"
+  },
+  {
+    "revision": "dad70c6b4f393277e7d2",
+    "url": "./static/js/2.a3037c5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.a3037c5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42f58b9d9e23af4e3cdf",
+    "url": "./static/js/main.a2905043.chunk.js"
+  },
+  {
+    "revision": "d6f9d1afe64e0585b830",
+    "url": "./static/js/runtime-main.8b27b102.js"
+  }
+]);
